4f7eea25e7d2a5b6726c7ae22af0e45b67dd4de7
yb <lojobs38@gmail.com>
2023-02-14 15:10:11 +0800

    Delete pages

--- website/src/index.tsx
+++
@@ -1,5 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
-import StartTesting from './StartTesting';
-
-render(() => <StartTesting />, document.getElementById('root') as HTMLElement);
